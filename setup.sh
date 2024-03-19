cefe40047f51612b5137ca51fff33065ced76359
durbanout13 <durbanout13@gmail.com>
2024-03-19 21:14:47 +0100

    updated base set up script

--- setup.sh
+++ setup.sh
@@ -4,11 +4,4 @@ if [[ $# -eq 0 ]] ; then
     echo "no project name provided..."
     exit 1
 fi
-
-mkdir $1
-pushd $1
-mkdir src
-cp ../build.sh .
-cp ../run.sh .
-cp ../.gitignore .
-popd
+cp -r base $1
